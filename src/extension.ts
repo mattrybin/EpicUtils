882ada3923858aef1b4db5cde39a5c68d401de6b
Matt Rybin <workmateuszrybin@gmail.com>
2023-03-13 03:45:25 +0100

    chore: update

--- src/extension.ts
+++ src/extension.ts
@@ -2,5 +2,5 @@ import { commands, ExtensionContext } from 'vscode';
 import { quickOpen } from './quickOpen';
 
 export function activate(context: ExtensionContext) {
-	context.subscriptions.push(commands.registerCommand('samples.quickInput', quickOpen));
+	context.subscriptions.push(commands.registerCommand('EpicUtils.changedFileNavigator', quickOpen));
 }
